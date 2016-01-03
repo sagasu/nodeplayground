c75d3a8c5275db148e61a3cc1ab21fbc5e2183fa
sagasu <sagasu@gmail.com>
2016-01-03 19:07:33 +0000

    Adding simple doing same thing http and express helloworld apps.

---
+++ js/httpApp.js
@@ -0,0 +1,10 @@
+//node ./js/httpApp.js
+//http://localhost:3000/
+
+var http = require("http");
+http.createServer(function(request,
+response){
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(3000);
