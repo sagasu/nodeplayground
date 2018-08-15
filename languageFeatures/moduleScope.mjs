0382d703a1e29f980a810e03800a2aa738df9eab
Mateusz Kopij <sagasu@gmail.com>
2018-08-15 14:01:26 +0100

    added explanation

--- languageFeatures/moduleScope.mjs
+++ languageFeatures/moduleScope.mjs
@@ -3,5 +3,6 @@ import {counter} from "./counters"
 //console.log(counter += 1);
 //TypeError: Assignment to constant variable.
 //at file:///C:/Users/sagasu/repos/nodeplayground/languageFeatures/moduleScope.mjs:3:21
+// because counter is not a variable it is immutable binding
 
 console.log(counter);
